403b2a005dde3ae1c86ae8d81525aa1527da5537
Apu59 <ssasarkar59@gmail.com>
2024-08-28 16:34:19 +0600

    Program to swap two numbers

--- exersice1.cpp
+++ exersice1.cpp
@@ -1,10 +1,25 @@
+//Program to swap two numbers'
+
 #include <bits/stdc++.h>
 using namespace std;
 
 
 int main (){
 
-        cout<<"Apu Sarker "<<endl;
+        int a = 5;
+        int b = 9;
+        int temp;
+
+        cout<<"Before swap "<<endl;
+        cout<<" a = "<<a<<" b = "<<b<<endl;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        cout<<"After swap "<<endl;
+        cout<<" a = "<<a<<" b = "<<b<<endl;
+
 
 
     return 0;
