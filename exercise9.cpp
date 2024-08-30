7f1a09163fe87d7b3fb7804bf57fcabc5e145d68
Apu59 <ssasarkar59@gmail.com>
2024-08-30 15:58:08 +0600

    Program to check whether the number is even or odd

---
+++ exercise9.cpp
@@ -0,0 +1,27 @@
+//
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main (){
+
+    int n;
+
+    cout<<"Please enter a number here : ";
+    cin>>n;
+
+    if(n % 2 == 0)
+    {
+        cout<<n<<" is an even number "<<endl;
+    }
+    else
+    {
+        cout<<n<<" is an odd number "<<endl;
+    }
+
+
+    return 0;
+}
