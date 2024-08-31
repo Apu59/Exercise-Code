b99f637adf31def2498779b4127a3d5f965e2cbc
Apu59 <ssasarkar59@gmail.com>
2024-08-31 22:13:45 +0600

    Print natural numbers

---
+++ exercise11.cpp
@@ -0,0 +1,22 @@
+//Print natural numbers in reverse order.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+
+    int num;
+
+    cout<<"Enter any number here : ";
+    cin>>num;
+
+    for (int i = num; i >= 1 ; i--)
+    {
+        cout<<i<<" ";
+    }
+    
+
+
+    return 0;
+}
