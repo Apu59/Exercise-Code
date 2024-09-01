faf3a43cb9e69c9f2e73ea811319540c2f95c581
Apu59 <ssasarkar59@gmail.com>
2024-09-01 22:05:40 +0600

    Program to count alphabets , digits and special characters in a string

---
+++ exercise14.cpp
@@ -0,0 +1,40 @@
+// Program to count alphabet , digits and special character is a string.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main (){
+
+        string str;
+        int alphabets = 0, digit = 0, spchar = 0;
+
+        cout<<"Enter a string here : ";
+        getline(cin, str);
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            if(isalpha(str[i]))
+            {
+                alphabets++;
+            }
+            else if (isdigit(str[i]))
+            {
+               digit++;
+            }
+            else
+            {
+                spchar++;
+            }
+            
+        }
+        cout<<"Total Alphabets : "<<alphabets<<endl;
+        cout<<"Total Digits : "<<digit<<endl;
+        cout<<"Total Special Characters "<<spchar<<endl;
+        
+
+
+
+    return 0;
+}
