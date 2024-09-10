64786432e344abdb32f3f5c981b2a643760bba15
Apu59 <ssasarkar59@gmail.com>
2024-09-10 12:05:21 +0600

    Program ot find length of string

---
+++ exercise23.cpp
@@ -0,0 +1,21 @@
+// Program to find length of string.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+
+        string str;
+        int lnt;
+
+        cout<<"Enter any string here : ";
+        getline(cin, str);
+
+        lnt = str.length();
+
+        cout<<"The length of this string is : "<<lnt<<endl;
+
+
+    return 0;
+}
