7cff4e11eb4e896f468701e8fe3aa1f1c8ccbc7c
Apu59 <ssasarkar59@gmail.com>
2024-09-10 11:37:14 +0600

    Program to print alphabet A to Z or a to z

---
+++ exercise20.cpp
@@ -0,0 +1,16 @@
+//Program to print alphabet A to Z or a to z;
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+
+        for(char letter = 'A'; letter <= 'Z'; letter++)  //The code will work for small alphabet also.
+        {
+            cout<<letter<< " ";
+        }
+
+
+    return 0;
+}
