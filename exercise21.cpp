1ea451126d01c251876efd644f461612b4a6be9a
Apu59 <ssasarkar59@gmail.com>
2024-09-10 11:42:25 +0600

    Program to add two integer number

---
+++ exercise21.cpp
@@ -0,0 +1,23 @@
+//Program to add two integer number.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+
+    int num1, num2, total;
+
+    cout<<"Enter first number here : ";
+    cin>>num1;
+    cout<<"Enter second number here : ";
+    cin>>num2;
+
+    total = num1 + num2;
+
+    cout<<"Sum of these two number is : "<<total<<endl;
+
+
+
+    return 0;
+}
