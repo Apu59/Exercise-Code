0ea06416f817014d25806ae85f10795ed6d3f722
Apu59 <ssasarkar59@gmail.com>
2024-09-16 12:35:28 +0600

    Liner Search Code

---
+++ exercise27.cpp
@@ -0,0 +1,36 @@
+//Liner Search
+
+#include<bits/stdc++.h>
+using namespace std;
+
+    int linerSearch(int arr[], int sz, int target)
+    {
+        for(int i = 0; i < sz; i++)
+        {
+            if(arr[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+int main(){
+
+    int arr[] = {2,4,6,8,3,5,7,9};
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    int result = linerSearch(arr, sz, target);
+
+    if(result != -1)
+    {
+        cout<<"Item found at index "<<result<<endl;
+    }
+    else
+    {
+        cout<<"Item is not found"<<endl;
+    }
+
+    return 0;
+}
