9bcc273182a6118fa24de990e098f9ff53435ef8
Apu59 <ssasarkar59@gmail.com>
2024-09-18 10:28:19 +0600

    Simple Interest

--- exercise31.cpp
+++ exercise31.cpp
@@ -5,8 +5,24 @@ using namespace std;
 
 int main (){
 
+    float A,r,P,t,simpleInterest;
+    
+
+    cout<<"Enter your principal balance : ";
+    cin>>P;
+
+    cout<<"Enter your time(in year) : ";
+    cin>>t;
+
+    cout<<"Enter the interest rate(in percentage) : ";
+    cin>>r;
+
+    simpleInterest = ((P*r*t)/100);
+    A = simpleInterest + P;
 
 
+    cout<<"The simple interest is : "<<simpleInterest<<endl;
+    cout<<"Your final balance is : "<<A<<endl;
 
 
     return 0;
