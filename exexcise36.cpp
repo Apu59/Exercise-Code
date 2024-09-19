61c1a1b653ce019521187b9124f203c7599fddd5
Apu59 <ssasarkar59@gmail.com>
2024-09-19 14:46:18 +0600

    Liner Search Practice

---
+++ exexcise36.cpp
@@ -0,0 +1,39 @@
+//Liner Search 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+    int linerSearch(int arr[], int sz, int target)
+    {
+        for(int i = 0; i < sz; i++)
+        {
+            if(arr[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+int main(){
+
+    int arr[] = {2,4,6,8,9,7,5};
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    int src;
+
+    cout<<"Enter the element : ";
+    cin>>src;
+
+    int result = linerSearch(arr, sz, src);
+
+    if(result != -1)
+    {
+        cout<<"Item found at index "<<result<<endl;
+    }
+    else 
+    {
+        cout<<"Item is not found "<<endl;
+    }
+
+    return 0;
+}
