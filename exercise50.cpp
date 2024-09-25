098f58fa3c08db3fbd82cadabcd2330d07036169
Apu59 <ssasarkar59@gmail.com>
2024-09-25 12:27:02 +0600

    Triangle pattern with stars

---
+++ exercise50.cpp
@@ -0,0 +1,23 @@
+//Triangle pattern 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    int n;
+    cout<<"Enter the value of n here : ";
+    cin>>n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < (i+1); j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+    return 0;
+}
