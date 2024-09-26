3f9ab4c26322099e1e7e55d2da7910ffd040ffca
Apu59 <ssasarkar59@gmail.com>
2024-09-26 14:38:49 +0600

    Floyd's triangle pattern character version

---
+++ exercise57.cpp
@@ -0,0 +1,26 @@
+// Floyd's triangle character version
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+        int n;
+        char ch = 'A';
+
+        cout<<"Enter the value of n here : ";
+        cin>>n;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < (i+1); j++){
+                cout<<ch<<" ";
+                ch++;
+            }
+            cout<<endl;
+        }
+
+
+
+    return 0;
+}
