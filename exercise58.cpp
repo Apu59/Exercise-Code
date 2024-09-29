aab5caaa2b1a6e3ca1c65866bba4e650ed99e388
Apu59 <ssasarkar59@gmail.com>
2024-09-29 13:36:08 +0600

    Inverted Triangle pattern

---
+++ exercise58.cpp
@@ -0,0 +1,24 @@
+//Inverted Triangle pattern
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+        int n; 
+
+        cout<<"Enter the value of n here : ";
+        cin>>n;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                cout<<" ";
+            }
+            for(int j = 0; j < (n-i); j++){
+                cout<<(i+1);
+            }
+            cout<<endl;
+        }
+
+    return 0;
+}
