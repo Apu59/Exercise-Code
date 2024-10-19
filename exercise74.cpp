48367be50582b5119fa82fe4773a157f4e328166
Apu59 <ssasarkar59@gmail.com>
2024-10-19 17:34:54 +0600

    Find the smallest element in the array

---
+++ exercise74.cpp
@@ -0,0 +1,38 @@
+// Find the smallest number in an array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    //Declare and define array from user input 
+        int n;
+        cout<<"Enter the size of the array : ";
+        cin>>n;
+
+        int size = n;
+        int nums[n];
+        for(int i = 0; i < size; i++){
+            cin>>nums[i];
+        }
+        cout<<endl;
+        cout<<"The array is : ";
+        for(int i = 0; i < size; i++){
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
+
+        // int nums[] = {12,54,65,9,-3,-15,-59,45,78,};
+        // int size = sizeof(nums) / sizeof(int);
+
+    //Find the smallest number in the array 
+        int smallest = INT_MAX;
+        for(int i = 0; i < size; i++){
+            if(nums[i] < smallest){
+                smallest = nums[i];
+            }
+        }
+            cout<<"The smallest number of the array is : "<<smallest<<endl;
+
+    return 0;
+}
