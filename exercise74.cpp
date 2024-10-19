c22c6de6ebf318a386dfac77a9ff3ac346641b07
Apu59 <ssasarkar59@gmail.com>
2024-10-19 17:56:50 +0600

    Find the smallest and largest number of an array

--- exercise74.cpp
+++ exercise74.cpp
@@ -29,13 +29,19 @@ int main (){
 
     //Find the smallest number in the array 
         int smallest = INT_MAX;
+        int largest = INT_MIN;
         for(int i = 0; i < size; i++){
             // smallest = min(nums[i] , smallest);  //We can use min function to compare between two numbers that which one is smallest. 
+            // largest = max(nums[i] , largest);   /We can use max function to compare between two numbers that which one is largest. 
             if(nums[i] < smallest){
                 smallest = nums[i];
             }
+            if(nums[i] > largest){
+                largest = nums[i];
+            }
         }
         cout<<"The smallest number of the array is : "<<smallest<<endl;
+        cout<<"The largest number of the array is : "<<largest<<endl;
 
     return 0;
 }
