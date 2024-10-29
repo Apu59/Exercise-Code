a839899acc1ac1830f676999c5c6aadd7aded901
Apu59 <ssasarkar59@gmail.com>
2024-10-29 14:37:21 +0600

    Liner search on vector

---
+++ exercise85.cpp
@@ -0,0 +1,29 @@
+//Liner Search on a vector.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int linerSearch(vector<int>& vec, int terget){
+        for(size_t i = 0; i < vec.size(); i++){  //size_t is unsinged int data type, it better to use insted of int in this case.
+            if(vec[i] == terget){
+                return i;
+            }
+        }
+        return -1;
+}
+
+int main (){
+
+        vector<int> numbers = {2,3,5,7,8,9};
+        int terget = 0;
+        int result = linerSearch(numbers,terget);
+        if(result != -1){
+            cout<<"Item found at index "<<result<<endl;
+        }else{
+            cout<<"Item is not found "<<endl;
+        }
+
+
+    return 0;
+}
