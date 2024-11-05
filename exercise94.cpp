b1716806b37c1d36872c36dcf1f0e82d958616db
Apu59 <ssasarkar59@gmail.com>
2024-11-05 14:41:59 +0600

    Majority element better approach

--- exercise94.cpp
+++ exercise94.cpp
@@ -1,16 +1,33 @@
 
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
+    int majorityElement(vector<int> nums){
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+        int freq = 1;
+        int ans = nums[0];
+        for(int i = 1; i < size; i++){
+            if(nums[i] == nums[i-1]){
+                freq++;
+            } else{
+                freq = 1;
+                ans = nums[i];
+            }
+            if(freq > size/2){
+                return ans;
+            }
+        }
+        return ans;
+    }
 
 int main (){
 
-
-cout<<"Apu Sarker ";
-
-
-
+    vector<int> vec = {2,1,2,1,1,2,2};
+    int result = majorityElement(vec);
+    cout<<result<<endl;
 
     return 0;
 }
