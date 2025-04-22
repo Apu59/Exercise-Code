37c12265c657e06da023dbc79883123df88873f0
Apu59 <ssasarkar59@gmail.com>
2025-04-22 15:44:40 +0600

    Find Nth fibonacci number by using Dynamic programming Top Down Approach

--- exercise144.cpp
+++ exercise144.cpp
@@ -1,19 +1,18 @@
+//Find nth fibonacci number by using Dynamic programming Top Down Approach(Recursion and Memoization)
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
 int fib(int n, vector<int>&dp){
-    if(n <= 1){
-        return n;
-    }
+    if(n <= 1) return n;
 
-    if(dp[n] != -1){
-        return dp[n];
-    }
+    if(dp[n] != -1) return dp[n];
 
-    dp[n] = fib(n-1, dp) + fib(n-2, dp);
+    dp[n] = fib(n-1,dp) + fib(n-2,dp);
     return dp[n];
 }
+
 
 int main(){
 
@@ -26,9 +25,7 @@ int main(){
         dp[i] = -1;
     }
 
-    cout<<fib(n,dp)<<endl;
-
-
+    cout<<"The "<<n<<"th fibonacci number is : "<<fib(n,dp)<<endl;
 
 
     return 0;
